728c18c07cfd7af501c05068e0122ba02c2f8290
fumi23 <78896035+fumi1623@users.noreply.github.com>
2021-08-09 15:48:24 +0900

    [Add]inMemory

--- src/main/java/SecurityConfig/SecurityConfig.java
+++ src/main/java/SecurityConfig/SecurityConfig.java
@@ -1,6 +1,7 @@
 package SecurityConfig;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -41,8 +42,35 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.passwordParameter("password") //ログインページのパスワード
 				.defaultSuccessUrl("/user/list", true); //成功後の転移先
 		
-		
 		//CSRF対策を無効に設定（一時的）
 		http.csrf().disable();
 	}
+	
+	// 認証の設定
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// インメモリ認証
+		auth
+			.inMemoryAuthentication()
+				.withUser("user") //userを追加
+					.password("user")
+					.roles("GENERAL")
+				.and()
+				.withUser("admin") //adminを追加
+					.password("admin")
+					.roles("ADMIN");
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
